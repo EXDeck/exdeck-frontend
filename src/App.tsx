bd80b0c89d0d94307e96d706799ef6d58ff64381
すえ <souhait.0614@gmail.com>
2022-07-25 08:09:36 +0900

    不要物の削除・lint

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,18 @@
-import { Component, createSignal } from 'solid-js';
-import './App.scss';
-import Counter from './Counter';
+import { Component, createSignal } from 'solid-js'
+
+import './App.scss'
 
 const App: Component = () => {
-  const [counter, setCounter] = createSignal(0);
-  setInterval(setCounter, 1000, (c: number) => c + 1);
+  const [counter, setCounter] = createSignal(0)
+  setInterval(setCounter, 1000, (c: number) => c + 1)
 
   return (
     <>
       <div>
         <h1 class="header">{counter()}</h1>
       </div>
-      <Counter />
     </>
-  );
-};
+  )
+}
 
-export default App;
+export default App
