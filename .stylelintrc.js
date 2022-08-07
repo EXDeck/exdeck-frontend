683947f345e6666ea0c2ba7814f4dbcd08b53148
すえ <souhait.0614@gmail.com>
2022-08-07 10:37:14 +0900

    stylelint導入

---
+++ .stylelintrc.js
@@ -0,0 +1,8 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  extends: ['stylelint-config-standard-scss', 'stylelint-config-recess-order'],
+  rules: {
+    'at-rule-no-unknown': null,
+    'scss/at-rule-no-unknown': true,
+  },
+}
