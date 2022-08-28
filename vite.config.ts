d4658b415f8af570035eedc3315b3bbd0ae8d445
すえ <souhait.0614@gmail.com>
2022-08-29 00:24:00 +0900

    ローカルでのHTTPS使用を実装

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,45 @@
+import { ServerOptions } from 'https'
 import path from 'path'
 
-import { defineConfig } from 'vite'
+import { certificateFor } from 'devcert'
+import { loadEnv, UserConfigExport } from 'vite'
 import eslint from 'vite-plugin-eslint'
 import solidPlugin from 'vite-plugin-solid'
 
-export default defineConfig({
-  plugins: [solidPlugin(), eslint()],
-  build: {
-    outDir: '../dist',
-    emptyOutDir: true,
-    target: 'esnext',
-  },
-  server: {
-    host: true,
-  },
-  root: './src',
-  publicDir: '../public',
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-      // BUG vite-plugin-solid-svgのimportパスの頭にsrcをつけてしまう不具合への暫定対処
-      $: __dirname,
+/**
+ * Create Cert
+ *
+ * @returns {Promise<ServerOptions>} 証明書情報
+ */
+async function createCert(): Promise<ServerOptions> {
+  const { key, cert } = await certificateFor('localhost')
+  return {
+    key,
+    cert,
+  }
+}
+
+export default async ({ mode }: { mode: string }): Promise<UserConfigExport> => {
+  process.env = { ...process.env, ...loadEnv(mode, `${process.cwd()}/src`) }
+  return {
+    plugins: [solidPlugin(), eslint()],
+    build: {
+      outDir: '../dist',
+      emptyOutDir: true,
+      target: 'esnext',
     },
-  },
-})
+    server: {
+      host: true,
+      https: !process.env.VITE_DEV_SERVER_USE_HTTPS ? false : await createCert(),
+    },
+    root: './src',
+    publicDir: '../public',
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+        // BUG vite-plugin-solid-svgのimportパスの頭にsrcをつけてしまう不具合への暫定対処
+        $: __dirname,
+      },
+    },
+  }
+}
