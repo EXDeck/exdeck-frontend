995942d4361ca91c0e838e20f929668b923a3e3c
すえ <souhait.0614@gmail.com>
2022-10-08 14:33:44 +0900

    簡略化+名称変更

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,19 @@ import { Main } from './pages/Main'
 import { Welcome } from './pages/Welcome'
 import { auth } from './scripts/backendApi'
 
-import './App.scss'
+import './base.scss'
 import '../node_modules/destyle.css'
 
 const App: Component = () => {
   const [status] = createResource(auth.getAuthStatus)
   // const [getIsLogIn] = createSignal(false)
   return (
-    <Switch>
+    <Switch fallback={<Welcome />}>
       <Match when={status.loading}>
         <span>Loading...</span>
       </Match>
       <Match when={status()?.signIn}>
         <Main />
-      </Match>
-      <Match when={!status()?.signIn}>
-        <Welcome />
       </Match>
     </Switch>
   )
